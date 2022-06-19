eb9096cbacdef4f795ea5aa6a89fd0d7343e484c
Ishan <you@example.com>
2022-06-20 02:14:51 +1000

    added counter functionality

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,17 +1,33 @@
 import React from "react";
+import '../index.css'
+
+
+
+
+
 
 export const Counter = function() {
+    const [counterValue, setCounterValue] = React.useState(0);
+
+    const addCounter = function() {
+        setCounterValue(counterValue + 1); // NEVER use counterValue++
+    }
+
+    const minusCounter = function() {
+        setCounterValue(counterValue - 1); // NEVER use counterValue--
+    }
+
 
 
 
 
     return (
         <div className="counter">
-            <button className="counter--minus">–</button>
+            <button className="counter--minus" onClick={minusCounter}>–</button>
             <div className="counter--count">
-                <h1>0</h1>
+                <h1>{counterValue}</h1>
             </div>
-            <button className="counter--plus">+</button>
+            <button className="counter--plus" onClick={addCounter}>+</button>
         </div>
     )
 }
